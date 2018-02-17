da82b4256f4c8d1ee742c8b4352a3578b44ced5e
Kevin Schaefer <kevin.schaefer@ibm.com>
2018-02-17 17:45:50 +0100

    Init Code

---
+++ config.h
@@ -0,0 +1,24 @@
+/*
+ *      Contains all "String" Data for the program
+*/
+
+// Welcome Message
+#define welcomeMessage "Welcome Forbes Billionaires List"
+
+
+// Menue Items
+#define menueItem1 "Add Billionaire"
+#define menueItem2 "Show Billionaires"
+#define menueItem3 "Delete Billionaire"
+#define menueItem4 "Sort Billionaires"
+#define menueItem5 "Save in file"
+#define menueItem6 "Load from file"
+#define menueItem7 "Search for ..."
+#define menueItem8 "Calculate memory"
+#define menueItem9 "Edit Billionaire"
+#define menueItem0 "Exit"
+
+
+
+
+
---
+++ main.c
@@ -0,0 +1,136 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "config.h"
+
+typedef enum { false, true } bool;
+
+struct billionaire_t {
+    // Contained Data
+    char name[128+1];
+    char surname[128+1];
+    float net_worth;
+    int selfmade_score;
+
+    // Pointers to next and previous list entry
+    struct billionaire_t* next;
+    struct billionaire_t* prev;
+};
+
+struct billionaire_t* balloc();
+struct billionaire_t* fillBillonaire(char name[], char surname[], float net_worth, int selfmade_score);
+
+void addBillionaire(struct billionaire_t* billionaire);
+void createBillionaire(char name[], char surname[], float net_worth, int selfmade_score);
+
+void printMenue(void);
+void handleInput(void);
+void handleExit(void);
+
+int getInput();
+
+struct billionaire_t *head;
+struct billionaire_t *tail;
+struct billionaire_t *current;
+
+int main(void) {
+    while(true) {
+        printMenue();
+        handleInput();
+    }
+}
+
+struct billionaire_t* balloc() {
+    return (struct billionaire_t*) malloc(sizeof(struct billionaire_t));;
+}
+
+struct billionaire_t* fillBillonaire(char name[], char surname[], float net_worth, int selfmade_score) {
+    struct billionaire_t* tmp = balloc();
+    strcpy(tmp->name, name);
+    strcpy(tmp->surname, name);
+    tmp->net_worth = net_worth;
+    tmp->selfmade_score = selfmade_score;
+    tmp->next = NULL;
+    tmp->prev = NULL;
+
+    return tmp;
+}
+
+void addBillionaire(struct billionaire_t* billionaire) {
+    struct billionaire_t* current = head;
+
+    if(head == NULL)
+        head = billionaire;
+
+    if(tail == NULL) {
+        tail = billionaire;
+    } else {
+        tail->next = billionaire;
+        billionaire->prev = tail;
+        tail = billionaire;
+    }
+}
+
+void createBillionaire(char name[], char surname[], float net_worth, int selfmade_score) {
+    struct billionaire_t* tmp = fillBillonaire(name, surname, net_worth, selfmade_score);
+    addBillionaire(tmp);
+}
+
+int getInput() {
+    int input = -1;
+
+    do {
+        printf("Selection: ");
+        scanf("%d", &input);
+    } while(input < 0 || input > 9);
+
+    return input;
+}
+
+void handleInput() {
+    int selection = getInput();
+
+    switch (selection) {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        case 7:
+            break;
+        case 8:
+            break;
+        case 9:
+            break;
+        case 0:
+            handleExit();
+        default:
+            break;
+    }
+}
+
+void handleExit() {
+    exit(0);
+}
+
+void printMenue(void) {
+    printf("==================================================\n");
+    printf("(1) - %s\n", menueItem1);
+    printf("(2) - %s\n", menueItem2);
+    printf("(3) - %s\n", menueItem3);
+    printf("(4) - %s\n", menueItem4);
+    printf("(5) - %s\n", menueItem5);
+    printf("(6) - %s\n", menueItem6);
+    printf("(7) - %s\n", menueItem7);
+    printf("(8) - %s\n", menueItem8);
+    printf("(9) - %s\n", menueItem9);
+    printf("(0) - %s\n", menueItem0);
+    printf("\n");
+}
