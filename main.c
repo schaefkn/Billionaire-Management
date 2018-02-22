12b367143e5c812149c08c5817772a48356f0acd
Kevin Schaefer <kevin.schaefer@ibm.com>
2018-02-22 14:34:02 +0100

    Adding Commentaries

--- main.c
+++ main.c
@@ -3,8 +3,14 @@
 #include <string.h>
 #include "config.h"
 
+/**
+ * Boolean Value declaration
+ */
 typedef enum { false, true } bool;
 
+/**
+ * Billionaire Struct to hold information about one Billionaire.
+ */
 struct billionaire_t {
     // Contained Data
     char name[128+1];
@@ -17,6 +23,9 @@ struct billionaire_t {
     struct billionaire_t* prev;
 };
 
+/**
+ * Billionaire Management Struct to manage head and tail.
+ */
 typedef struct billionaire_management {
     struct billionaire_t* head;
     struct billionaire_t* tail;
@@ -33,6 +42,7 @@ void deleteAllBillionairesFromLinkedList(BillionaireManagement *bm);
 void editBillionaireFromList(int index, BillionaireManagement *bm);
 void swapBillionaireWithNextBillionaire(int index, BillionaireManagement *bm);
 void sortBillionairesByCategory(int category, BillionaireManagement *bm);
+void loadBillionairesFromFile(char file_name[], BillionaireManagement *bm);
 bool billionaireComesBefore(int category, struct billionaire_t* first, struct billionaire_t* second);
 bool searchForProperty(int property, struct billionaire_t* current, char searchFor[]);
 
@@ -58,12 +68,17 @@ void printBillionareProperties(struct billionaire_t* billionaire, int number, bo
 
 void handleInput(BillionaireManagement *bm);
 void handleExit(BillionaireManagement *bm);
+void handleAddBillionaire(char name[], char surname[], float net_worth, int selfmade_score, BillionaireManagement *bm);
+void handleSaveBillionairesToFile(char file_name[], BillionaireManagement *bm);
 
 int getInput(int min, int max);
 
 const char* getfield(char* line, int num);
 
-
+/**
+ * Inserts sample data
+ * @param bm BillionaireManagement Pointer
+ */
 void insertSampleData(BillionaireManagement *bm) {
     createBillionaire("Bill", "Gates", 91.7, 8, bm);
     createBillionaire("Jeff", "Bezos", 120.8, 8, bm);
@@ -72,22 +87,43 @@ void insertSampleData(BillionaireManagement *bm) {
     createBillionaire("Alpha", "Centaury", 201, 10, bm);
 }
 
-int main(void) {
-    printGreeting();
+/**
+ * Main method
+ */
+int main(int argc, const char* argv[]) {
     BillionaireManagement bm;
     bm.head = NULL;
     bm.tail = NULL;
 
+    if(argc == 2) {
+        char file_name[128+1];
+        strcpy(file_name, argv[1]);
+        loadBillionairesFromFile(file_name, &bm);
+    }
+
+    printGreeting();
     while(true) {
         printMenu();
         handleInput(&bm);
     }
 }
 
+/**
+ * Allocates memory for a Billionaire and gives back to pointer to the memory space.
+ * @return struct billionaire_t*
+ */
 struct billionaire_t* balloc(void) {
     return (struct billionaire_t*) malloc(sizeof(struct billionaire_t));;
 }
 
+/**
+ * Fills a Billionaire struct with supplied data.
+ * @param name First Name of Billionaire
+ * @param surname Last Name of Billionaire
+ * @param net_worth Net-Worth of Billionaire
+ * @param selfmade_score Selfmade-Score of Billionaire
+ * @return struct billionaire_t*
+ */
 struct billionaire_t* fillBillonaire(char name[], char surname[], float net_worth, int selfmade_score) {
     struct billionaire_t* tmp = balloc();
     strcpy(tmp->name, name);
@@ -100,6 +136,12 @@ struct billionaire_t* fillBillonaire(char name[], char surname[], float net_wort
     return tmp;
 }
 
+/**
+ * Gets Billionaire by supplied index. If index is greater than the length of the list , NULL will be returned.
+ * @param index Index from Billionaire
+ * @param bm BillionaireManagement Pointer
+ * @return struct billionaire_t*
+ */
 struct billionaire_t* getBillionaireByIndex(int index, BillionaireManagement *bm) {
     if(index == 0) {
         return bm->head;
@@ -118,6 +160,11 @@ struct billionaire_t* getBillionaireByIndex(int index, BillionaireManagement *bm
     }
 }
 
+/**
+ * Adds a supplied billionaire to the linked list from BillionaireManagement struct.
+ * @param billionaire Billionaire to be added to linked list.
+ * @param bm BillionaireManagement Pointer
+ */
 void addBillionaireToLinkedList(struct billionaire_t *billionaire, BillionaireManagement *bm) {
     if(bm->head == NULL)
         bm->head = billionaire;
@@ -131,11 +178,25 @@ void addBillionaireToLinkedList(struct billionaire_t *billionaire, BillionaireMa
     }
 }
 
+/**
+ * Creates a billionaire and adds them to the linked list.
+ * @param name First Name of Billionaire
+ * @param surname Last Name of Billionaire
+ * @param net_worth Net-Worth of Billionaire
+ * @param selfmade_score Selfmade-Score of Billionaire
+ * @param bm
+ */
 void createBillionaire(char name[], char surname[], float net_worth, int selfmade_score, BillionaireManagement *bm) {
     struct billionaire_t* tmp = fillBillonaire(name, surname, net_worth, selfmade_score);
     addBillionaireToLinkedList(tmp, bm);
 }
 
+/**
+ * Deletes a Billionaire from the linked list by supplied index. If there is no Billionaire at this index, an error is printed.
+ * @param index Index of Billionaire to be deleted.
+ * @param printDeletion Should the method print deletion to the console
+ * @param bm BillionaireManagement Pointer
+ */
 void deleteBillionaireFromList(int index, bool printDeletion, BillionaireManagement *bm) {
     struct billionaire_t* toDelete = getBillionaireByIndex(index, bm);
     char name[128+1];
@@ -168,10 +229,19 @@ void deleteBillionaireFromList(int index, bool printDeletion, BillionaireManagem
     }
 }
 
+/**
+ * Gets the allocated memory from the linked list.
+ * @param bm BillionaireManagement Pointer
+ */
 void getMemorySizeAllocated(BillionaireManagement *bm) {
     printf("%lu Bytes allocated\n", getLengthOfLinkedList(bm) * sizeof(struct billionaire_t));
 }
 
+/**
+ * Gets length of linked list.
+ * @param bm BillionaireManagement Pointer
+ * @return int length
+ */
 int getLengthOfLinkedList(BillionaireManagement *bm) {
     struct billionaire_t* current = bm->head;
     int i = 1;
@@ -188,6 +258,13 @@ int getLengthOfLinkedList(BillionaireManagement *bm) {
     return i;
 }
 
+
+/**
+ * Gets Input from User, if input is not between min and max user is prompted again, as lang as input is invalid.
+ * @param min Minimum Input
+ * @param max Maximum Intput
+ * @return Int
+ */
 int getInput(int min, int max) {
     int input = -1;
 
@@ -199,6 +276,10 @@ int getInput(int min, int max) {
     return input;
 }
 
+/**
+ * Gets input and handles input accordingly.
+ * @param bm BillionaireManagement Pointer
+ */
 void handleInput(BillionaireManagement *bm) {
     int selection = getInput(0, 9);
 
@@ -237,17 +318,28 @@ void handleInput(BillionaireManagement *bm) {
     }
 }
 
+/**
+ * Exit Routines
+ * Deletes all Billionaires from list and frees the allocated memory, then handles exits.
+ * @param bm BillionaireManagement Pointer
+ */
 void handleExit(BillionaireManagement *bm) {
     deleteAllBillionairesFromLinkedList(bm);
     exit(0);
 }
 
+/**
+ * Prints greetings message to the screen.
+ */
 void printGreeting(void) {
     printf("==================================================\n");
     printf("          Welcome Forbes Billionaires List        \n");
     printf("                   Version: 0.0.1                 \n");
 }
 
+/**
+ * Prints menu to the screen.
+ */
 void printMenu(void) {
     printf("==================================================\n");
     printf("                      MENUE                       \n");
@@ -265,6 +357,10 @@ void printMenu(void) {
     printWhitespaceOnce();
 }
 
+/**
+ * Prints the menu for adding a Billionaire to the linked list. And asks the user for input.
+ * @param bm BillionaireManagement Pointer
+ */
 void printAddBillionaireMenu(BillionaireManagement *bm) {
     char name[128+1];
     char surname[128+1];
@@ -283,8 +379,20 @@ void printAddBillionaireMenu(BillionaireManagement *bm) {
     printf("Selfmade-Score: ");
     scanf("%d", &selfmade_score);
 
+    handleAddBillionaire(name, surname, net_worth, selfmade_score, bm);
+}
+
+/**
+ * Handles the creation and adding from a billionaire to the linked list, after the add billionaire menu is executed. Later asks the user if he wants to add another billionaire.
+ * @param name First Name of Billionaire
+ * @param surname Last Name of Billionaire
+ * @param net_worth Net-Worth of Billionaire
+ * @param selfmade_score Selfmade-Score of Billionaire
+ * @param bm BillionaireManagement Pointer
+ */
+void handleAddBillionaire(char name[], char surname[], float net_worth, int selfmade_score, BillionaireManagement *bm) {
     createBillionaire(name, surname, net_worth, selfmade_score, bm);
-    
+
     printWhitespaceOnce();
     printf("%s %s was added to the list of Billionaires!\n", name, surname);
     printf("Do you want to add another Billionaire ? y/n: ");
@@ -301,6 +409,10 @@ void printAddBillionaireMenu(BillionaireManagement *bm) {
     printWhitespaceOnce();
 }
 
+/**
+ * Prints all Billionaires in the linked list to the console.
+ * @param bm BillionaireManagement Pointer
+ */
 void printShowBillionairesMenu(BillionaireManagement *bm) {
     printf("==================================================\n");
     printf("                WORLDS BILLIONAIRES               \n");
@@ -309,6 +421,10 @@ void printShowBillionairesMenu(BillionaireManagement *bm) {
     printAllBillionares(false, bm);
 }
 
+/**
+ * Prints the delete Billionaire menu and prompts the user which Billionaire should ne deleted. After that calls the appropretiate function to delete the selected Billionaire.
+ * @param bm BillionaireManagement Pointer
+ */
 void printDeleteBillionairesMenu(BillionaireManagement *bm) {
     printf("==================================================\n");
     printf("                DELETE BILLIONAIRE                \n");
@@ -322,6 +438,10 @@ void printDeleteBillionairesMenu(BillionaireManagement *bm) {
     }
 }
 
+/**
+ * Prints the edit Billionaire menu and prompts the user for which property he/she wants to edit. After that calls the appropriate function to edit the supplied property.
+ * @param bm BillionaireManagement Pointer
+ */
 void printEditBillionairesMenu(BillionaireManagement *bm) {
     printf("==================================================\n");
     printf("                 EDIT BILLIONAIRE                 \n");
@@ -335,6 +455,10 @@ void printEditBillionairesMenu(BillionaireManagement *bm) {
     }
 }
 
+/**
+ * Prints the sort Billionaire menu and prompts the user for which property the list should be sorted. After that calls the appropriate function to sort the linked list.
+ * @param bm
+ */
 void printSortBillionairesMenu(BillionaireManagement *bm) {
     printf("==================================================\n");
     printf("                 SORT BILLIONAIRE                 \n");
@@ -351,6 +475,10 @@ void printSortBillionairesMenu(BillionaireManagement *bm) {
     sortBillionairesByCategory(input, bm);
 }
 
+/**
+ * Prints the search for Billionaire menu and prompts the user for which property to search for.
+ * @param bm BillionaireManagement Pointer
+ */
 void printSearchForBillionairesMenu(BillionaireManagement *bm) {
     printf("==================================================\n");
     printf("                SEARCH BILLIONAIRE                \n");
@@ -385,6 +513,10 @@ void printSearchForBillionairesMenu(BillionaireManagement *bm) {
     }
 }
 
+/**
+ * Prints the menu for saving the linked list the file. Prompts the user for input and calls the appropriate function for saving the data.
+ * @param bm BillionaireManagement Pointer
+ */
 void printSaveInFileMenu(BillionaireManagement *bm) {
     printf("==================================================\n");
     printf("                SAVE BILLIONAIRES                 \n");
@@ -394,8 +526,16 @@ void printSaveInFileMenu(BillionaireManagement *bm) {
     char file_name[128+1];
     scanf("%s", file_name);
     sprintf(file_name, "%s.csv", file_name);
+    handleSaveBillionairesToFile(file_name, bm);
+    printWhitespace(3);
+}
 
-
+/**
+ * Handles the saving from the linked list to file
+ * @param file_name File name of the file which linked list should be saved to
+ * @param bm BillionaireMangement Pointer
+ */
+void handleSaveBillionairesToFile(char file_name[], BillionaireManagement *bm) {
     FILE *fileToSaveTo = fopen(file_name, "w");
     if(fileToSaveTo == NULL) {
         printf("Fatal error trying to open file!");
@@ -412,16 +552,22 @@ void printSaveInFileMenu(BillionaireManagement *bm) {
         char selfmade_score[8];
         sprintf(selfmade_score, "%d", current->selfmade_score);
 
+        // Saves Billionaire accordingly to csv
         fprintf(fileToSaveTo, "%s;%s;%s;%s\n", current->name, current->surname, net_worth, selfmade_score);
     }
 
     fclose(fileToSaveTo);
     printWhitespaceOnce();
-    printf("Sucessfully saved to file: %s\n", file_name);
-    printWhitespace(3);
+    printf("Successfully saved to file: %s\n", file_name);
 }
 
 
+/**
+ * Gets a field from the supplied line which is in csv format.
+ * @param line Line to read data from
+ * @param num Index of the field to be loaded
+ * @return String
+ */
 const char* getfield(char* line, int num)
 {
     const char* tok;
@@ -435,12 +581,20 @@ const char* getfield(char* line, int num)
     return NULL;
 }
 
+/**
+ * Delets all Billionaires from the linked list
+ * @param bm BillionaireManagement Pointer
+ */
 void deleteAllBillionairesFromLinkedList(BillionaireManagement *bm) {
     for (int i = getLengthOfLinkedList(bm) - 1; i >= 0; i--) {
         deleteBillionaireFromList(i, false, bm);
     }
 }
 
+/**
+ * Prints the load billionaires from file menu.
+ * @param bm BillionaireManagement Pointer
+ */
 void printLoadFromFileMenu(BillionaireManagement *bm) {
     printf("==================================================\n");
     printf("                LOAD BILLIONAIRES                 \n");
@@ -450,9 +604,19 @@ void printLoadFromFileMenu(BillionaireManagement *bm) {
     char file_name[128+1];
     scanf("%s", file_name);
     sprintf(file_name, "%s.csv", file_name);
-
     printWhitespaceOnce();
 
+    loadBillionairesFromFile(file_name, bm);
+    printf("Successfully loaded from file: %s\n", file_name);
+    printWhitespace(3);
+}
+
+/**
+ * Loads Billionares from supplied file name and inserts them into the linked list.
+ * @param file_name File name from which Billionaires should ne loaded from.
+ * @param bm BillionaireManagement Pointer
+ */
+void loadBillionairesFromFile(char file_name[], BillionaireManagement *bm) {
     FILE *fileToOpen = fopen(file_name, "r");
     if(fileToOpen == NULL) {
         printf("There is no file with name: %s\n", file_name);
@@ -481,10 +645,17 @@ void printLoadFromFileMenu(BillionaireManagement *bm) {
 
         free(tmp);
     }
+
 }
 
 
-
+/**
+ * Search if the supplied searchFor string matches the supplied category from supplied Billionaire.
+ * @param property Property to be checked for
+ * @param current Billionaire to be checked for
+ * @param searchFor String which Billionaire should contain in supplied category
+ * @return bool
+ */
 bool searchForProperty(int property, struct billionaire_t* current, char searchFor[]) {
 
     char net_worth[64];
@@ -519,6 +690,11 @@ bool searchForProperty(int property, struct billionaire_t* current, char searchF
     }
 }
 
+/**
+ * Edits the supplied property of the supplied Billionaire.
+ * @param index Index of Billionaire to be edited
+ * @param bm BillionaireManagement Pointer
+ */
 void editBillionaireFromList(int index, BillionaireManagement *bm) {
     struct billionaire_t* billionaireToEdit = getBillionaireByIndex(index, bm);
     printBillionareProperties(billionaireToEdit, 0, false);
@@ -551,6 +727,11 @@ void editBillionaireFromList(int index, BillionaireManagement *bm) {
     }
 }
 
+/**
+ * Sorts the linked list by the supplied category
+ * @param category  Category to be checked (1) for Fist Name (2) for Second name (3) for Net-Worth and (4) for Selfmade-Score
+ * @param bm BillionaireManagement Pointer
+ */
 void sortBillionairesByCategory(int category, BillionaireManagement *bm) {
     for(int i = getLengthOfLinkedList(bm); i > 1; i--) {
         for(int n = 0; n < i -1; n++) {
@@ -562,6 +743,13 @@ void sortBillionairesByCategory(int category, BillionaireManagement *bm) {
     }
 }
 
+/**
+ * Checks of the supplied category data from the first Billionare comes before the second.
+ * @param category Category to be checked (1) for Fist Name (2) for Second name (3) for Net-Worth and (4) for Selfmade-Score
+ * @param first First Billionaire -> to be checked
+ * @param second Second Billionaire -> to be compared to
+ * @return bool
+ */
 bool billionaireComesBefore(int category, struct billionaire_t* first, struct billionaire_t* second) {
     if(first == NULL || second == NULL)
         return false;
@@ -592,22 +780,41 @@ bool billionaireComesBefore(int category, struct billionaire_t* first, struct bi
     }
 }
 
-
+/**
+ * Swaps a Billionaire with the one after it
+ * @param index Index of Billionaire to be swapped
+ * @param bm BillionaireManagement Pointer
+ */
 void swapBillionaireWithNextBillionaire(int index, BillionaireManagement *bm) {
+    // Billionaire to be swapped.
     struct billionaire_t* billionaireToSwap = getBillionaireByIndex(index, bm);
+    // Billionare after the element to be swapped. (Will be swapped with the element toswap)
     struct billionaire_t* billionaireAfter = billionaireToSwap->next;
 
+    // If there is no element after the one to be swapped. The element can't be swapped.
+    // So we need to check if there is an element, after the eleent to be swapped.
     if(billionaireAfter) {
+        // Before [billionaireToSwap] <--> [billionaireAfter]
+        // First switch the the elements themselves.
         billionaireToSwap->next = billionaireAfter->next;
         billionaireAfter->prev = billionaireToSwap->prev;
         billionaireAfter->next = billionaireToSwap;
         billionaireToSwap->prev = billionaireAfter;
 
+
+        // Now [billionaireAfter] <--> [billionaireToSwap]
+        // Checks if the next element of toswap exists (previously the next elemtn of after),
+        // if so set its prev to swap.
         if(billionaireToSwap->next) billionaireToSwap->next->prev = billionaireToSwap;
+        // Checks the previous element of after exists (prevously the prev elemtn of toswap),
+        // if so sets its next to after.
         if(billionaireAfter->prev) billionaireAfter->prev->next = billionaireAfter;
+        // If the old after element was the tail element, now after the swap the new tail must be toswap.
         if(billionaireAfter == bm->tail) bm->tail = billionaireToSwap;
+        // The the old swap element was the head element, now aftehr the swap the new head must be after.
         if(billionaireToSwap == bm->head) bm->head = billionaireAfter;
     }
+    // If there is no element after the one we want to swap, we do nothing.
 }
 
 int printAllBillionares(bool shortened, BillionaireManagement *bm) {
