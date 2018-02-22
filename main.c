924b67cfe31e84dbfaea0f34247f1f454239d773
Kevin Schaefer <kevin.schaefer@ibm.com>
2018-02-22 16:45:27 +0100

    Added secret easter egg

--- main.c
+++ main.c
@@ -70,6 +70,7 @@ void handleInput(BillionaireManagement *bm);
 void handleExit(BillionaireManagement *bm);
 void handleAddBillionaire(char name[], char surname[], float net_worth, int selfmade_score, BillionaireManagement *bm);
 void handleSaveBillionairesToFile(char file_name[], BillionaireManagement *bm);
+void handleSecretEasterEgg(BillionaireManagement *bm);
 
 int getInput(int min, int max);
 
@@ -276,7 +277,7 @@ int getInput(int min, int max) {
 
     do {
         printf("Selection: ");
-    } while (((scanf("%d%c", &input, &c) !=2 || c!='\n') && clean_stdin()) || input < min || input > max);
+    } while (((scanf("%d%c", &input, &c) !=2 || c!='\n') && clean_stdin()) || ((input < min || input > max) && input != 42));
 
     return input;
 }
@@ -318,6 +319,8 @@ void handleInput(BillionaireManagement *bm) {
             break;
         case 0:
             handleExit(bm);
+        case 42:
+            handleSecretEasterEgg(bm);
         default:
             break;
     }
@@ -339,7 +342,7 @@ void handleExit(BillionaireManagement *bm) {
 void printGreeting(void) {
     printf("==================================================\n");
     printf("          Welcome Forbes Billionaires List        \n");
-    printf("                   Version: 0.0.1                 \n");
+    printf("                   Version: 1.0.1                 \n");
 }
 
 /**
@@ -617,7 +620,7 @@ void printLoadFromFileMenu(BillionaireManagement *bm) {
 }
 
 /**
- * Loads Billionares from supplied file name and inserts them into the linked list.
+ * Loads Billionares from supplied file name, deletes the linked list and inserts them into the new linked list.
  * @param file_name File name from which Billionaires should ne loaded from.
  * @param bm BillionaireManagement Pointer
  */
@@ -898,4 +901,87 @@ void printLineBreak(int times) {
     for (int i = 0; i < times; ++i) {
         printf("\n");
     }
+}
+
+
+void handleSecretEasterEgg(BillionaireManagement *bm) {
+    printLineBreak(6);
+
+    printf("                                .do-\"\"\"\"\"'-o..                         \n"
+                   "                             .o\"\"            \"\"..                       \n"
+                   "                           ,,''                 ``b.                   \n"
+                   "                          d'                      ``b                   \n"
+                   "                         d`d:                       `b.                 \n"
+                   "                        ,,dP                         `Y.               \n"
+                   "                       d`88                           `8.               \n"
+                   " ooooooooooooooooood888`88'                            `88888888888bo, \n"
+                   "d\"\"\"    `\"\"\"\"\"\"\"\"\"\"\"\"Y:d8P                              8,          `b \n"
+                   "8                    P,88b                             ,`8           8 \n"
+                   "8                   ::d888,                           ,8:8.          8 \n"
+                   ":                   dY88888                           `' ::          8 \n"
+                   ":                   8:8888                               `b          8 \n"
+                   ":                   Pd88P',...                     ,d888o.8          8 \n"
+                   ":                   :88'dd888888o.                d8888`88:          8 \n"
+                   ":                  ,:Y:d8888888888b             ,d88888:88:          8 \n"
+                   ":                  :::b88d888888888b.          ,d888888bY8b          8 \n"
+                   "                    b:P8;888888888888.        ,88888888888P          8 \n"
+                   "                    8:b88888888888888:        888888888888'          8 \n"
+                   "                    8:8.8888888888888:        Y8888888888P           8 \n"
+                   ",                   YP88d8888888888P'          \"\"888888\"Y            8 \n"
+                   ":                   :bY8888P\"\"\"\"\"''                     :            8 \n"
+                   ":                    8'8888'                            d            8 \n"
+                   ":                    :bY888,                           ,P            8 \n"
+                   ":                     Y,8888           d.  ,-         ,8'            8 \n"
+                   ":                     `8)888:           '            ,P'             8 \n"
+                   ":                      `88888.          ,...        ,P               8 \n"
+                   ":                       `Y8888,       ,888888o     ,P                8 \n"
+                   ":                         Y888b      ,88888888    ,P'                8 \n"
+                   ":                          `888b    ,888888888   ,,'                 8 \n"
+                   ":                           `Y88b  dPY888888OP   :'                  8 \n"
+                   ":                             :88.,'.   `' `8P-\"b.                   8 \n"
+                   ":.                             )8P,   ,b '  -   ``b                  8 \n"
+                   "::                            :':   d,'d`b, .  - ,db                 8 \n"
+                   "::                            `b. dP' d8':      d88'                 8 \n"
+                   "::                             '8P\" d8P' 8 -  d88P'                  8 \n"
+                   "::                            d,' ,d8'  ''  dd88'                    8 \n"
+                   "::                           d'   8P'  d' dd88'8                     8 \n"
+                   " :                          ,:   `'   d:ddO8P' `b.                   8 \n"
+                   " :                  ,dooood88: ,    ,d8888\"\"    ```b.                8 \n"
+                   " :               .o8\"'\"\"\"\"\"\"Y8.b    8 `\"''    .o'  `\"\"\"ob.           8 \n"
+                   " :              dP'         `8:     K       dP''        \"`Yo.        8 \n"
+                   " :             dP            88     8b.   ,d'              ``b       8 \n"
+                   " :             8.            8P     8\"\"'  `\"                 :.      8 \n"
+                   " :            :8:           :8'    ,:                        ::      8 \n"
+                   " :            :8:           d:    d'                         ::      8 \n"
+                   " :            :8:          dP   ,,'                          ::      8 \n"
+                   " :            `8:     :b  dP   ,,                            ::      8 \n"
+                   " :            ,8b     :8 dP   ,,                             d       8 \n"
+                   " :            :8P     :8dP    d'                       d     8       8 \n"
+                   " :            :8:     d8P    d'                      d88    :P       8 \n"
+                   " :            d8'    ,88'   ,P                     ,d888    d'       8 \n"
+                   " :            88     dP'   ,P                      d8888b   8        8 \n"
+                   " '           ,8:   ,dP'    8.                     d8''88'  :8        8 \n"
+                   "             :8   d8P'    d88b                   d\"'  88   :8        8 \n"
+                   "             d: ,d8P'    ,8P\"\"\".                      88   :P        8 \n"
+                   "             8 ,88P'     d'                           88   ::        8 \n"
+                   "            ,8 d8P       8                            88   ::        8 \n"
+                   "            d: 8P       ,:  -hrr-                    :88   ::        8 \n"
+                   "            8',8:,d     d'                           :8:   ::        8 \n"
+                   "           ,8,8P'8'    ,8                            :8'   ::        8 \n"
+                   "           :8`' d'     d'                            :8    ::        8 \n"
+                   "           `8  ,P     :8                             :8:   ::        8 \n"
+                   "            8, `      d8.                            :8:   8:        8 \n"
+                   "            :8       d88:                            d8:   8         8 \n"
+                   " ,          `8,     d8888                            88b   8         8 \n"
+                   " :           88   ,d::888                            888   Y:        8 \n"
+                   " :           YK,oo8P :888                            888.  `b        8 \n"
+                   " :           `8888P  :888:                          ,888:   Y,       8 \n"
+                   " :            ``'\"   `888b                          :888:   `b       8 \n"
+                   " :                    8888                           888:    ::      8 \n"
+                   " :                    8888:                          888b     Y.     8, \n"
+                   " :                    8888b                          :888     `b     8: \n"
+                   " :                    88888.                         `888,     Y     8: \n"
+                   " ``ob...............--\"\"\"\"\"'----------------------`\"\"\"\"\"\"\"\"'\"\"\"`'\"\"\"\"\"\n");
+
+    printLineBreak(6);
 }
